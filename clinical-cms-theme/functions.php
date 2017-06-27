6835625b64bb811de3fa4db39bcf114b7a195c05
codeclinic <phill@codeclinic.de>
2017-06-27 20:54:21 +0200

    sldr rv rqrd

--- clinical-cms-theme/functions.php
+++ clinical-cms-theme/functions.php
@@ -162,7 +162,7 @@ function clinical_cms_theme_register_required_plugins() {
 		array(
 			'name'               => 'Slider Revolution', // The plugin name.
 			'slug'               => 'revslider', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/inc/tgmps/plugins/slider-revolution.zip', // The plugin source.
+			'source'             => get_template_directory() . '/inc/tgmpa/plugins/slider-revolution.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
