012398cf7ccacb645cd36b8191bcfa31de113c5e
codeclinic <phill@codeclinic.de>
2017-07-03 21:51:11 +0200

    ftr

--- clinical-cms-theme/footer.php
+++ clinical-cms-theme/footer.php
@@ -15,6 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+            <!--
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clinical-cms-theme' ) ); ?>"><?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'clinical-cms-theme' ), 'WordPress' );
@@ -24,6 +25,7 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'clinical-cms-theme' ), 'clinical-cms-theme', '<a href="https://automattic.com/">Code Clinic KreativAgentur</a>' );
 			?>
+            -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
