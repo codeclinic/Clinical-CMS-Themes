b2bca54f51a82eb6ec681866c24f769a0d30d5c6
codeclinic <phill@codeclinic.de>
2017-07-05 21:07:22 +0200

    pg tmplt

--- clinical-cms-theme/page.php
+++ clinical-cms-theme/page.php
@@ -13,36 +13,34 @@
  */
 
 get_header(); ?>
-<div class="vc_row wpb_row vc_row-fluid">
-    <div class="wpb_column vc_column_container vc_col-sm-2">
-        <div class="vc_column-inner">
-            <div class="wpb_wrapper">LEFT PANEL</div>
+<div class="wpb_column vc_column_container vc_col-sm-2">
+    <div class="vc_column-inner">
+        <div class="wpb_wrapper">LEFT PANEL</div>
+    </div>
+</div>
+<div class="wpb_column vc_column_container vc_col-sm-8">
+    <div id="primary" class="content-area vc_column-inner">
+        <div id="main" class="site-main wpb_wrapper">
+
+                    <?php
+                    while ( have_posts() ) : the_post();
+
+                        get_template_part( 'template-parts/content', 'page' );
+
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+
+                    endwhile; // End of the loop.
+                    ?>
+
         </div>
     </div>
-    <div class="wpb_column vc_column_container vc_col-sm-8">
-        <div id="primary" class="content-area vc_column-inner">
-            <div id="main" class="site-main wpb_wrapper">
-                
-                        <?php
-                        while ( have_posts() ) : the_post();
-
-                            get_template_part( 'template-parts/content', 'page' );
-
-                            // If comments are open or we have at least one comment, load up the comment template.
-                            if ( comments_open() || get_comments_number() ) :
-                                comments_template();
-                            endif;
-
-                        endwhile; // End of the loop.
-                        ?>
-
-            </div>
-        </div>
-    </div>
-    <div class="wpb_column vc_column_container vc_col-sm-2">
-        <div class="vc_column-inner">
-            <div class="wpb_wrapper">RIGHT PANEL</div>
-        </div>
+</div>
+<div class="wpb_column vc_column_container vc_col-sm-2">
+    <div class="vc_column-inner">
+        <div class="wpb_wrapper">RIGHT PANEL</div>
     </div>
 </div>
 <?php
