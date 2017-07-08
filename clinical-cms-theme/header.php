ca02f89a2dbe5cb4e4c18f0c36f99b40158dba63
codeclinic <phill@codeclinic.de>
2017-07-08 13:26:27 +0200

    hdr

--- clinical-cms-theme/header.php
+++ clinical-cms-theme/header.php
@@ -24,7 +24,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'clinical-cms-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+<!--		<div class="site-branding">
             <?php
 /*
 			if ( is_front_page() && is_home() ) : ?>
@@ -41,17 +41,18 @@
 			endif; 
 */    
     ?>
-		</div><!-- .site-branding -->
+		</div><!-- .site-branding -- >
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'clinical-cms-theme' ); ?></button>
-			<?php
+			<? php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				) );
 			?>
-		</nav><!-- #site-navigation -->
+		</nav><!-- #site-navigation -- >
+-->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
