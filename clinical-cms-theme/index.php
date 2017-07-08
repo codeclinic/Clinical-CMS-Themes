8ba82e86f23eb3160b400e9d0d1882af17c107de
codeclinic <phill@codeclinic.de>
2017-07-08 20:03:43 +0200

    pst tmplt tst

--- clinical-cms-theme/index.php
+++ clinical-cms-theme/index.php
@@ -18,7 +18,7 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 		<?php
-        $page_for_posts_id = get_option( 'page_for_posts' );
+        $page_for_posts_id = /* get_option( 'page_for_posts' ) */ '12';
         $page_for_posts_obj = get_post( $page_for_posts_id );
         echo "CONTENT" . apply_filters( 'the_content', $page_for_posts_obj->post_content );
         
