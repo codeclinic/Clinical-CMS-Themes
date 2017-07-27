70be0b5b3da5a86c00112dffa5a816898262cdf5
codeclinic <phill@codeclinic.de>
2017-07-27 23:05:44 +0200

    sdbr tmplt php fk

---
+++ clinical-cms-theme/sidebar.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The sidebar fake template to satisfy WP Theme requirements
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Clinical_CMS_Theme
+ */
