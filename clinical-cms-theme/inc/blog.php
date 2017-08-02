51f08cf223362c25c860f30ab02d1400f679bf61
codeclinic <phill@codeclinic.de>
2017-08-02 12:23:13 +0100

    iugiugbuib j90ij ioh

--- clinical-cms-theme/inc/blog.php
+++ clinical-cms-theme/inc/blog.php
@@ -176,12 +176,48 @@ if(!function_exists('clinical_cms_theme_blog_block')){
     add_shortcode('Clinical_CMS_Theme_Blog_Block', 'clinical_cms_theme_blog_block');
 }
 
-if(!function_exists('clinical_cms_theme_blog_header')) {
-	function clinical_cms_theme_blog_headers( $atts, $content =  null) {
-       return '<header class="entry-header">' . do_shortcode($content) . '</header><!-- .entry-header -->';
+if(!function_exists('single_carousel_content')) {
+	function single_carousel_content( $atts, $content =  null) {
+		extract(shortcode_atts(array(
+			'title' => 'Flexible & Customizable',
+			'description' => '',
+			'url' => '',
+			'img' => ''
+		), $atts));
+        
+        $url = ($url=='||') ? '' : $url;
+		$url = ps_build_link( $url );
+		$a_link = $url['url'];
+		$a_title = ($url['title'] == '') ? '' : 'title="'.$url['title'].'"';
+		$a_target = ($url['target'] == '') ? '' : 'target="'.$url['target'].'"';
+        $button = $a_link ? '<a class="btn btn-md btn-black" href="'.$a_link. '" '.$a_title.' '.$a_target.'>'.$url['title'].'</a>' : '';
+
+        $image = wp_get_attachment_image_src( $img, 'full');
+		$image_src = $image['0'];
+        
+        
+        $output = '<div class="item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 mb-sm-30">
+                                <img src="'.$image_src.'" alt="" />
+                            </div>
+                            <div class="col-md-5 col-md-offset-1">
+                                <h3>'.$title.'</h3>
+                                <div class="spacer-15"></div>
+                                '.$description.'
+                                <div class="spacer-15"></div>
+                                '.$button.'
+                            </div>
+                        </div>
+                    </div>
+                </div>'; 
+        
+        return $output;
 	}
-	add_shortcode('Clinical_Cms_Theme_Blog_Headers', 'clinical_cms_theme_blog_headers');		
+	add_shortcode('single_carousel_content', 'single_carousel_content');		
 }
+
 
 
 
@@ -199,15 +235,33 @@ vc_map( array(
 ) );
 
 vc_map( array(
-    "name" => __("Single Carousel Content", "clinical-cms-theme"),
-    "base" => "Clinical_Cms_Theme_Blog_Headers",
-    "content_element" => false,
-    "as_child" => array('only' => 'Clinical_CMS_Theme_Blog_Block'),
-    "show_settings_on_create" => false,
-    /*
-    "is_container" => false,
-    "js_view" => 'VcColumnView',
-    */
+    "name" => __("Single Carousel Content", "mozel"),
+    "base" => "single_carousel_content",
+    "content_element" => true,
+    "as_child" => array('only' => 'carousel_content'),
+    "show_settings_on_create" => true,
+    "params" => array(
+        array(
+            "type" => "textfield",
+            "heading" => __("Title", "mozel"),
+            "param_name" => "title"
+        ), 
+        array(
+            "type" => "textarea",
+            "heading" => __("Description", "mozel"),
+            "param_name" => "description"
+        ),
+        array(
+			'type' => 'vc_link',
+			'heading' => __( 'Button', 'mozel' ),
+			'param_name' => 'url',
+		),
+        array(
+			'type' => 'attach_image',
+			'heading' => __( 'Add Image', 'mozel' ),
+			'param_name' => 'img',
+		)
+    ),
 ) );
 
 if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
@@ -218,7 +272,7 @@ if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
 }
 
 if ( class_exists( 'WPBakeryShortCode' ) ) {
-    class WPBakeryShortCode_Clinical_Cms_Theme_Blog_Header extends WPBakeryShortCode {
+    class WPBakeryShortCode_Single_Carousel_Content extends WPBakeryShortCode {
     }
 }
 
