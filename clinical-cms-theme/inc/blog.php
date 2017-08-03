65a3f2968d317af24d7e1d9f698e9786f9e528df
codeclinic <phill@codeclinic.de>
2017-08-03 12:27:07 +0100

    tbsb rtshr rth

--- clinical-cms-theme/inc/blog.php
+++ clinical-cms-theme/inc/blog.php
@@ -206,7 +206,7 @@ if(!function_exists('clinical_cms_theme_blog_Title')) {
 vc_map( array(
     "name" => __("Clinical CMS Blog Block", "clinical-cms-theme"), 
     "base" => "Clinical_CMS_Theme_Blog_Block",
-    "as_parent" => array('only' => 'Clinical_CMS_Theme_Blog_Header_Start', 'Clinical_CMS_Theme_Blog_Header_Close', 'only' => 'Clinical_CMS_Theme_Blog_Title'),
+    "as_parent" => array('only' => 'Clinical_CMS_Theme_Blog_Header_Start', 'Clinical_CMS_Theme_Blog_Header_Close', 'Clinical_CMS_Theme_Blog_Title'),
     "content_element" => true,
     "show_settings_on_create" => false,
     "is_container" => true,
