762b0070ec426e7502d94301bcbe2234091547f1
codeclinic <phill@codeclinic.de>
2017-08-09 15:25:10 +0100

    blg lyt

--- clinical-cms-theme/template-parts/content.php
+++ clinical-cms-theme/template-parts/content.php
@@ -11,7 +11,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php  
-        $page_for_posts_obj = get_post( 68 );
+        $titan = TitanFramework::getInstance( 'clinical-cms-theme' );
+        $page_for_posts_ID = $titan->getOption( 'aa_sec_body_bg_clr' );
+        $page_for_posts_obj = get_post( $page_for_posts_ID );
+        //$page_for_posts_obj = get_post( 68 );
         echo apply_filters( 'the_content', $page_for_posts_obj->post_content );
 ?>
 </article><!-- #post-<?php the_ID(); ?> -->
