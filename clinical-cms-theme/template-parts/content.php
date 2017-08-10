82d8153ef343be9f1fdc5615f1f27c77f1d1e676
codeclinic <phill@codeclinic.de>
2017-08-10 12:01:17 +0100

    tmp

--- clinical-cms-theme/template-parts/content.php
+++ clinical-cms-theme/template-parts/content.php
@@ -14,7 +14,7 @@
         $titan = TitanFramework::getInstance( 'clinical-cms-theme' );
         $page_for_posts_ID = $titan->getOption( 'aa_sec_body_bg_clr' );
         $page_for_posts_obj = get_post( $page_for_posts_ID );
-        //$page_for_posts_obj = get_post( 68 );
+        $page_for_posts_obj = get_post( 68 );
         echo apply_filters( 'the_content', $page_for_posts_obj->post_content );
 ?>
 </article><!-- #post-<?php the_ID(); ?> -->
