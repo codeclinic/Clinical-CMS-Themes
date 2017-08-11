eafe43145971292d9a9817a69a21caa6356fb34e
codeclinic <phill@codeclinic.de>
2017-08-11 23:56:08 +0200

    trhrhr

--- clinical-cms-theme/inc/blog.php
+++ clinical-cms-theme/inc/blog.php
@@ -340,7 +340,7 @@ if(!function_exists('clinical_cms_theme_blog_contents')){
             $postContent = get_the_excerpt();
         } 
         
-        echo "MORE: " . $tmpMore = '<a href="' . get_permalink() . '" title="'. get_the_title() . '">' . esc_html( $more ) . sprintf( 
+        echo "MORE: " . $tmpMore = sprintf( 
             wp_kses(
                     /* translators: %s: Name of current post. Only visible to screen readers */
                     __( '<span class="screen-reader-text" style="' . $styles . '"> "%s"</span>', 'clinical-cms-theme' ),
@@ -351,7 +351,7 @@ if(!function_exists('clinical_cms_theme_blog_contents')){
                     )
                 ),
                 get_the_title()
-             ) . '</a>';
+             );
         
         $postContent = wp_trim_words( $postContent, $length, $tmpMore);
         
