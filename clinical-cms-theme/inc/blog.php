c431bb362897fe19e071ebe481b5296edd54c5b6
codeclinic <phill@codeclinic.de>
2017-08-12 19:37:28 +0200

    mr lnk

--- clinical-cms-theme/inc/blog.php
+++ clinical-cms-theme/inc/blog.php
@@ -331,7 +331,7 @@ if(!function_exists('clinical_cms_theme_blog_contents')){
             the_content( sprintf( 
                 wp_kses(
                         /* translators: %s: Name of current post. Only visible to screen readers */
-                        __( '$more<span class="screen-reader-text" style="' . $styles . '"> "%s"</span>', 'clinical-cms-theme' ),
+                        __( $more . '<span class="screen-reader-text" style="' . $styles . '"> "%s"</span>', 'clinical-cms-theme' ),
                         array(
                             'span' => array(
                             'class' => array(),
