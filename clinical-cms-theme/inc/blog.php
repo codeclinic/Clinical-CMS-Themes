84dec5e0be30146dedac3cc8439d571b5a419930
codeclinic <phill@codeclinic.de>
2017-08-13 22:16:00 +0200

    fxs

--- clinical-cms-theme/inc/blog.php
+++ clinical-cms-theme/inc/blog.php
@@ -327,7 +327,7 @@ if(!function_exists('clinical_cms_theme_blog_contents')){
         
         /*$safeMore = sprintf(  
                 wp_kses(
-                        /* translators: %s: Name of current post. Only visible to screen readers */
+                        /* translators: %s: Name of current post. Only visible to screen readers * /
                         esc_html ( $more ) . __( '<span class="screen-reader-text" style="' . $styles . '"> "%s"</span>', 'clinical-cms-theme' ),
                         array(
                             'span' => array(
